ae5729bbadc7809009253dab5de48bee1dfdb51c
Roberto Sanchez <rosanchez@uamv.edu.ni>
2020-09-01 17:30:37 -0600

    Mis Cambios

---
+++ WebServiceProject/Controllers/SalesController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebServiceProject.Models;
+
+
+namespace WebServiceProject.Controllers
+{
+    public class SalesController : Controller
+    {
+        // GET: Sales
+        public ActionResult GetAll()
+        {
+            List<SalesCLS> salesList = null;
+            using (var db = new AdventureWorksLT2019Entities1())
+            {
+                salesList = (List<SalesCLS>)(from SalesOrder in db.SalesOrderHeaders
+                                             join Detail in db.SalesOrderDetails
+                                          on SalesOrder.SalesOrderID equals Detail.SalesOrderID
+                                             join Product in db.Products
+                                             on Detail.ProductID equals Product.ProductID
+
+
+                                             select new SalesCLS
+                                             {
+                                                 SalesOrderID = SalesOrder.SalesOrderID,
+                                                 Status = SalesOrder.Status,
+                                                 DueDate = SalesOrder.DueDate,
+                                                 ShipDate = (DateTime)SalesOrder.ShipDate,
+                                                 OrderDate = SalesOrder.OrderDate,
+                                                 OrderSubTotal = SalesOrder.SubTotal,
+                                                 TaxAmt = SalesOrder.TaxAmt,
+                                                 Freight = SalesOrder.Freight,
+                                                 Name = Product.Name,
+                                                 OrderQty = Detail.OrderQty
+                                             }).ToList();
+            }
+            return View(salesList);
+        }
+    }
+}
---
+++ WebServiceProject/Models/SalesCLS.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebServiceProject.Models
+{
+    public class SalesCLS
+    {
+        [Display(Name = "Order Id")]
+        public int SalesOrderID { get; set; }
+
+        [Display(Name = "Due Date")]
+        public DateTime DueDate { get; set; }
+
+        [Display(Name = "Ship Date")]
+        public DateTime ShipDate { get; set; }
+        [Display(Name = "Order Date")]
+
+        public DateTime OrderDate { get; set; }
+
+        [Display(Name = "Status")]
+        public byte Status { get; set; }
+
+        [Display(Name = "Sub Total")]
+        public decimal OrderSubTotal { get; set; }
+
+        [Display(Name = "Tax Amount")]
+        public decimal TaxAmt { get; set; }
+
+        [Display(Name = "Freight")]
+        public decimal Freight { get; set; }
+
+        [Display(Name = "Product Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Order Quantity")]
+        public decimal OrderQty { get; set; }
+    }
+}
